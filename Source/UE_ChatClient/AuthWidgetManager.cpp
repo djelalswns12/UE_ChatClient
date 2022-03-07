95d7c3b637c26e387fef0f77454348c92fc08cef
djelalswns12 <djelalswns12@naver.com>
2022-03-07 22:02:05 +0900

    로그인버튼 콜백함수 구현
    로그인버튼 콜백함수 구현

---
+++ Source/UE_ChatClient/AuthWidgetManager.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuthWidgetManager.h"
+
+
+void UAuthWidgetManager::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 위젯 블루프린트의 버튼을 이름을 통해 가져온다
+	LoginBtn = Cast<UButton>(GetWidgetFromName(TEXT("LoginBtn")));
+
+	// 위젯 블루프린트의 텍스트 블록을 이름을 통해 가져온다
+	LoginInputTextBox = Cast<UEditableTextBox>(GetWidgetFromName(TEXT("LoginInputTextBox")));
+
+	// 버튼 클릭시 호출될 델리게이트에 함수를 등록한다
+	LoginBtn->OnClicked.AddDynamic(this, &UAuthWidgetManager::TestButtonCallback);
+}
+
+void UAuthWidgetManager::LoginCallback()
+{
+	UE_LOG(LogTemp, Log, TEXT("%s"), *LoginInputTextBox->Text.ToString());
+}
---
+++ Source/UE_ChatClient/AuthWidgetManager.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableTextBox.h"
+
+#include "AuthWidgetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_CHATCLIENT_API UAuthWidgetManager : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		class UButton* LoginBtn;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		class UEditableTextBox* LoginInputTextBox;
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	// 버튼을 눌렀을 때, 호출될 델리게이트에 등록할 함수
+	UFUNCTION(BlueprintCallable)
+		void LoginCallback();
+};
