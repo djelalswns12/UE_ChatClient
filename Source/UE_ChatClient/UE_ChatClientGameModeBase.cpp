4a36c5c1eddc7a8a72c4fcc5bfe8aa896362881a
djelalswns12 <djelalswns12@naver.com>
2022-03-07 18:03:21 +0900

    Auth Widget 구현중
    Auth Widget 디자인
    
    gamebase에 Widget 전환 함수 구현

--- Source/UE_ChatClient/UE_ChatClientGameModeBase.cpp
+++ Source/UE_ChatClient/UE_ChatClientGameModeBase.cpp
@@ -3,3 +3,35 @@
 
 #include "UE_ChatClientGameModeBase.h"
 
+//AUE_ChatClientGameModeBase::AUE_ChatClientGameModeBase() {
+//	static ConstructorHelpers::FClassFinder<UUserWidget> widget(TEXT("/Game/AuthWidget"));
+//	if (widget.Succeeded() && widget.Class != NULL) 
+//	{
+//		 StartingWidgetClass=widget.Class;
+//	}
+//}
+
+void AUE_ChatClientGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+
+}
+
+void AUE_ChatClientGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
+
--- Source/UE_ChatClient/UE_ChatClientGameModeBase.h
+++ Source/UE_ChatClient/UE_ChatClientGameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include <Blueprint/UserWidget.h>
 #include "UE_ChatClientGameModeBase.generated.h"
 
 /**
@@ -13,5 +14,17 @@ UCLASS()
 class UE_CHATCLIENT_API AUE_ChatClientGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable, Category = "UMG_Game")
+		void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG_Game")
+		TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	UPROPERTY()
+		UUserWidget* CurrentWidget;
+
 };
