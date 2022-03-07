fc684ec41c20794e7a43053e2376fddc50f185da
djelalswns12 <djelalswns12@naver.com>
2022-03-07 15:09:06 +0900

    Init
    Init

---
+++ Source/UE_ChatClient/UE_ChatClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_ChatClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_CHATCLIENT_API AUE_ChatClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
